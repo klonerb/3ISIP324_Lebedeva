ca09bfbdc78b7ecbbe4acc0590dd827695feccd5
klonerb <e.kwwi6@gmail.com>
2026-09-17 13:03:30 +0300

    Update Program.cs

--- 3ISIP324_Lebedeva/Program.cs
+++ 3ISIP324_Lebedeva/Program.cs
@@ -9,7 +9,19 @@ namespace _3ISIP324_Lebedeva
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            List<Product> products = new List<Products>();
+ int nextId = 1;
+ string[] categories = { "Продукты", "Электроника", "Одежда" };
+ while (true);
+    {
+     Console.WriteLine("\n===== УЧЕТ ТОВАРОВ В МАГАЗИНЕ =====");
+     Console.WriteLine("1. Добавить товар");
+     Console.WriteLine("2. Удалить товар");
+     Console.WriteLine("3. Заказать поставку товра");
+     Console.WriteLine("4. Продать товар");
+     Console.WriteLine("5. Поиск товаров");
+     Console.WriteLine("6.Показать все товары");
         }
     }
 }
